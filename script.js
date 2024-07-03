2f6133a9b870199df04a386cbbce225bae8b5a70
Kabera Samuel <kaberanshutis@gmail.com>
2024-07-03 12:28:48 +0200

    Testing a Second Solution
    Trying another solution for creating user preferred grid resolution
    This is after I found that my grid won't work for every resolution the user chose

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 // 1. Creating grids
-const createDivs = function (ele) {
-  for (let outerCounter = 1; outerCounter <= 16; outerCounter++) {
-    for (let innerCounter = 1; innerCounter <= 16; innerCounter++) {
+const createDivs = function (ele, counts) {
+  const size = 500 / counts;
+  for (let outerCounter = 1; outerCounter <= counts; outerCounter++) {
+    for (let innerCounter = 1; innerCounter <= counts; innerCounter++) {
+      // creating individual grid
       const div = document.createElement("div");
-      div.textContent = "";
+      div.style.cssText = `width: ${size}px; height: ${size}px`;
       div.classList.add("inline");
       ele.appendChild(div);
 
       // adding a line break
-      if (innerCounter === 16) {
+      if (innerCounter === counts) {
         const lineBreak = document.createElement("br");
         ele.appendChild(lineBreak);
       }
@@ -17,12 +19,20 @@ const createDivs = function (ele) {
 };
 const main = document.querySelector("main");
 
-createDivs(main);
+createDivs(main, 20);
 // 2. Implementing drawing
 const inlineList = document.querySelectorAll(".inline");
-
 for (let inline of inlineList) {
   inline.addEventListener("mouseenter", () => {
     inline.classList.add("hoverColor");
   });
 }
+
+// 3. Implementing choosing grid size
+// const input = document.querySelector("input");
+// input.addEventListener("keypress", (e) => {
+//   if (e.key === "Enter") {
+//     createDivs(main, Number(input.value));
+//     console.log(typeof input.value);
+//   }
+// });
