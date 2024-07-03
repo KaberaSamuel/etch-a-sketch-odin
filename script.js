d86b387f10e7beab173e97713da7aad3545875b9
Kabera Samuel <kaberanshutis@gmail.com>
2024-07-03 12:28:48 +0200

    Testing a Second Solution
    Trying another solution for creating user preferred grid resolution
    This is after I found that my grid won't work for every resolution the user chose

--- script.js
+++ script.js
@@ -1,28 +1,25 @@
 // 1. Creating grids
-const createDivs = function (ele) {
-  for (let outerCounter = 1; outerCounter <= 16; outerCounter++) {
-    for (let innerCounter = 1; innerCounter <= 16; innerCounter++) {
+const createDivs = function (ele, counts) {
+  let size = 500 / counts;
+
+  // looping and creating grids
+  for (let outerCounter = 1; outerCounter <= counts; outerCounter++) {
+    for (let innerCounter = 1; innerCounter <= counts; innerCounter++) {
+      // creating individual grid
       const div = document.createElement("div");
-      div.textContent = "";
+      div.style.cssText = `width: ${size}px; height: ${size}px`;
       div.classList.add("inline");
       ele.appendChild(div);
-
-      // adding a line break
-      if (innerCounter === 16) {
-        const lineBreak = document.createElement("br");
-        ele.appendChild(lineBreak);
-      }
     }
   }
 };
 const main = document.querySelector("main");
+createDivs(main, 20);
 
-createDivs(main);
 // 2. Implementing drawing
 const inlineList = document.querySelectorAll(".inline");
-
 for (let inline of inlineList) {
   inline.addEventListener("mouseenter", () => {
-    inline.classList.add("hoverColor");
+    div.classList.add("hoverColor");
   });
 }
