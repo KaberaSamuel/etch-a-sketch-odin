3fe5652049b61c275ba1c4aaa1814a06231bf35f
Kabera Samuel <kaberanshutis@gmail.com>
2024-07-04 11:25:48 +0200

    Adding feature of choosing preferred resolution

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 // 1. Creating grids
 const createDivs = function (ele, counts) {
-  let size = 500 / counts;
-
+  let size = 400 / counts;
   // looping and creating grids
   for (let outerCounter = 1; outerCounter <= counts; outerCounter++) {
     for (let innerCounter = 1; innerCounter <= counts; innerCounter++) {
@@ -9,18 +8,32 @@ const createDivs = function (ele, counts) {
       const div = document.createElement("div");
       div.style.cssText = `width: ${size}px; height: ${size}px`;
       div.classList.add("inline");
+      div.addEventListener("mouseenter", () => {
+        div.classList.add("hoverColor");
+      });
       ele.appendChild(div);
     }
   }
 };
 
+// Node list of all grid divs
+const allGridDivs = document.querySelectorAll(".inline");
+console.log(allGridDivs);
+
 const main = document.querySelector("main");
 createDivs(main, 10);
 
-// 2. Implementing drawing
-const inlineList = document.querySelectorAll(".inline");
-for (let inline of inlineList) {
-  inline.addEventListener("mouseenter", () => {
-    inline.classList.add("hoverColor");
-  });
-}
+// 2. Implementing user preferred resolution+
+const input = document.querySelector("input");
+input.addEventListener("keypress", (event) => {
+  const value = Number(input.value);
+  if (event.key === "Enter") {
+    if (value > 1 && value < 71) {
+      main.textContent = "";
+      createDivs(main, value);
+    } else {
+      alert(input.placeholder);
+      console.log(value, typeof value);
+    }
+  }
+});
