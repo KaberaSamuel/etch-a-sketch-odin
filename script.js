e04619ced1dea2d94e79b8c7201a6a77dd28e973
Kabera Samuel <kaberanshutis@gmail.com>
2024-07-10 01:35:06 +0200

    Introducing LoopEdit function
    Aim of this function is to reduce the repetition of the for loop done while determing the background to be displayed for an individual grid item

--- script.js
+++ script.js
@@ -1,15 +1,34 @@
-// Node list of all grid divs
+// Global variables
 let allGridDivs = "";
+let resolution = 25;
+const main = document.querySelector("main");
 
 // Functions
-const resetGrid = function (resolution = 20) {
-  main.textContent = "";
-  createDivs(main, resolution);
+const loopEdit = function (command) {
+  for (let div of allGridDivs) {
+    div.addEventListener("mouseenter", () => {
+      // 1.
+      if (command === "Erase") {
+        div.style.backgroundColor = "white";
+      }
+
+      // 2.
+      else if (command === "Random") {
+        div.style.backgroundColor = randomColor();
+      }
+
+      // 3.
+      else if (command === "Black") {
+        div.style.backgroundColor = "black";
+      }
+    });
+  }
 };
 
 // 1. Creating grids
-const createDivs = function (ele, counts) {
-  let size = 400 / counts;
+const createGrids = function (counts = resolution) {
+  main.textContent = "";
+  const size = 400 / counts;
   // looping and creating grids
   for (let outerCounter = 1; outerCounter <= counts; outerCounter++) {
     for (let innerCounter = 1; innerCounter <= counts; innerCounter++) {
@@ -18,27 +37,28 @@ const createDivs = function (ele, counts) {
       div.style.cssText = `width: ${size}px; height: ${size}px`;
       div.classList.add("inline");
       div.addEventListener("mouseenter", () => {
-        div.classList.add("hoverColor");
+        div.style.backgroundColor = "black";
       });
-      ele.appendChild(div);
+      main.appendChild(div);
     }
   }
   allGridDivs = document.querySelectorAll(".inline");
 };
 
-const main = document.querySelector("main");
-createDivs(main, 20);
+createGrids();
 
 // 2. Implementing user preferred resolution
 const input = document.querySelector("input");
-let inputChoice = 20;
 input.addEventListener("keypress", (event) => {
-  let value = Number(input.value);
   if (event.key === "Enter") {
-    if (value > 1 && value < 91) {
-      inputChoice = value;
-      resetGrid(value);
+    resolution = Number(input.value);
+    if (resolution > 1 && resolution < 91) {
+      // setting initial values
+      createGrids(resolution);
+      randombtn.textContent = "Random";
+      draw_erase.textContent = "Erase";
     } else {
+      resolution = 25;
       alert("Sorry, Allowed range is from 2 to 90");
     }
     input.value = "";
@@ -46,28 +66,29 @@ input.addEventListener("keypress", (event) => {
 });
 
 // 3. Implementing erasing and drawing
-const draw_erase = document.querySelector("#draw-erase");
+const draw_erase = document.querySelector("#erase");
 draw_erase.addEventListener("click", () => {
   if (draw_erase.textContent === "Erase") {
     draw_erase.textContent = "Draw";
-    for (let div of allGridDivs) {
-      div.addEventListener("mouseenter", () => {
-        div.classList.remove("hoverColor");
-      });
-    }
+    loopEdit("Erase");
   } else {
     draw_erase.textContent = "Erase";
-    for (let div of allGridDivs) {
-      div.addEventListener("mouseenter", () => {
-        div.classList.add("hoverColor");
-      });
+    if (randombtn.textContent === "Random") {
+      loopEdit("Black");
+    } else {
+      loopEdit("Random");
     }
   }
 });
 
 // 4. Implementing resetting functionality
 const resetbtn = document.querySelector("#reset");
-resetbtn.addEventListener("click", resetGrid);
+resetbtn.addEventListener("click", () => {
+  resolution = 15;
+  randombtn.textContent = "Random";
+  draw_erase.textContent = "Erase";
+  createGrids();
+});
 
 // 5. Implementing random color functionality
 const randomNumber = function (min, max) {
@@ -84,20 +105,12 @@ const randomColor = function () {
 
 const randombtn = document.querySelector("#random");
 randombtn.addEventListener("click", () => {
-  resetGrid(inputChoice);
+  createGrids();
   if (randombtn.textContent === "Random") {
     randombtn.textContent = "Black";
-    for (let div of allGridDivs) {
-      div.addEventListener("mouseenter", () => {
-        div.style.backgroundColor = randomColor();
-      });
-    }
+    loopEdit("Random");
   } else {
     randombtn.textContent = "Random";
-    for (let div of allGridDivs) {
-      div.addEventListener("mouseenter", () => {
-        div.style.backgroundColor = "black";
-      });
-    }
+    loopEdit("Black");
   }
 });
